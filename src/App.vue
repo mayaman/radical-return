ad2b9748737e5bd82d2d1fd26c27079bf3404fd6
mayaman26 <mayaman26@gmail.com>
2021-09-07 21:31:53 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,179 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <transition name="fade">
+      <TextBorder
+        :key="siteData[index].url"
+        class="radical"
+        :text="siteData[index].url"
+        size="680"
+      />
+    </transition>
+
+    <transition name="fade">
+      <TextBorder
+        :key="siteData[index].date"
+        class="radical"
+        :text="'Visited on ' + siteData[index].date"
+        size="430"
+      />
+    </transition>
+
+    <div class="radical stroke outer-shape">
+      <div class="radical stroke inner-shape">
+        <iframe
+          id="past-url"
+          :key="siteData[index].url"
+          :src="siteData[index].url"
+        ></iframe>
+      </div>
+    </div>
+
+    <button @click="updateSite()">回</button>
+    <!-- <button v-for="(data, index) in siteData" @click="updateSite(index)">
+      {{ siteData[index].date }}
+    </button> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TextBorder from "./components/TextBorder.vue";
+import siteDataJSON from "./assets/data.json";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TextBorder,
+  },
+  data: function () {
+    return {
+      index: 0,
+      prevIndex: 1,
+      siteData: siteDataJSON.siteData,
+    };
+  },
+  mounted() {
+    // console.log(this.siteData);
+    // setInterval(() => {
+    //   this.updateSite();
+    // }, 10000);
+  },
+  methods: {
+    updateSite(index) {
+      this.index = (this.index + 1) % this.siteData.length;
+      if (this.index == 0) {
+        this.prevIndex = this.siteData.length - 1;
+      } else {
+        this.prevIndex = this.index - 1;
+      }
+
+      // If there are individual buttons
+      // this.index = index;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background: gray;
+  /* background: url('https://cdn.pixabay.com/photo/2017/03/17/19/37/sky-2152463_1280.jpg'); */
+  background: url("https://unleash.org/wp-content/uploads/2017/01/sky-1280x720.jpg")
+    no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  background-position: fixed;
+  user-select: none;
+  -webkit-user-select: none;
+  color: white;
+  font-size: 18px;
+  line-height: 24px;
+  overflow: hidden;
 }
+
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+
+.radical {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+iframe {
+  /* -webkit-transform: scale(0.2);
+    transform: scale(0.2); */
+  /* width: 100%;
+    height: 100%; */
+  border: none;
+  width: 200% !important;
+  height: 200% !important;
+  -webkit-transform: scale(0.5);
+  transform: scale(0.5);
+  -webkit-transform-origin: 0 0;
+  transform-origin: 0 0;
+}
+
+.stroke {
+  border: 5px solid white;
+}
+
+.outer-shape {
+  width: 600px;
+  height: 600px;
+}
+
+.inner-shape {
+  width: 350px;
+  height: 350px;
+  overflow: hidden;
+  background: white;
+}
+
+button:hover {
+  cursor: w-resize;
+}
+
+/* .fade-enter-active,
+.fade-leave-active {
+  transition: opacity 3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+} */
+
+/* .slide-fade-enter-active {
+  transition: all 0.5s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.5s cubic-bezier(1, 0, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+{
+  transform: translateX(-100px);
+  opacity: 0;
+} */
+
+/* .bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+} */
 </style>
---
+++ src/components/TextBorder.vue
@@ -0,0 +1,75 @@
+<template>
+  <div
+    class="border"
+    v-bind:style="{ width: size + 'px', height: size + 'px' }"
+  >
+    <fieldset>
+      <legend>{{ text }}</legend>
+    </fieldset>
+    <fieldset>
+      <legend>{{ text }}</legend>
+    </fieldset>
+    <fieldset>
+      <legend>{{ text }}</legend>
+    </fieldset>
+    <fieldset>
+      <legend>{{ text }}</legend>
+    </fieldset>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextBorder",
+  props: {
+    text: String,
+    size: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div.border {
+  display: grid;
+  margin: auto;
+  font-style: italic;
+}
+
+fieldset {
+  border: 1px solid transparent;
+  border-top-color: #000000;
+  box-sizing: border-box;
+  grid-area: 1 / 1;
+  /* padding: 20px; */
+  border: none;
+  width: inherit;
+}
+
+fieldset:nth-of-type(1) {
+  background: content-box center/contain no-repeat;
+}
+
+fieldset:nth-of-type(2) {
+  transform: rotate(90deg);
+}
+
+fieldset:nth-of-type(3) {
+  transform: rotate(180deg);
+}
+
+fieldset:nth-of-type(4) {
+  transform: rotate(-90deg);
+}
+
+legend {
+  /* font: 15pt/0 'Averia Serif Libre'; */
+  margin: auto;
+  padding: 5px 0px;
+  /* background: hotpink; */
+}
+
+fieldset:nth-of-type(3) > legend {
+  transform: rotate(180deg);
+}
+</style>
